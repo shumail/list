4371caf3ec65cf209343f14541f8654ed81aac99
shaharyar <shaharyar_92@rocketmail.com>
2014-04-16 15:23:55 +0500

    commit

---
+++ Implicit_Intend/src/com/example/implicit_intend/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.implicit_intend;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+
+import android.widget.ImageView;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		ImageView iv=(ImageView) findViewById(R.id.imageView1);
+		iv.setImageURI((Uri) getIntent().getExtras().get(Intent.EXTRA_STREAM));
+	}
+
+	
+
+}
